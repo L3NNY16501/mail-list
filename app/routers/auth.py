f5775afbf7ba9ed3007faf3247aca5201ddc6d3c
Ryan Leonard <ryanlfx16@gmail.com>
2026-02-25 11:13:07 +0000

    Added authentication endpoints

---
+++ app/routers/auth.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from app import models, schemas, security, crud
+from app.database import get_db
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"]
+)
+
+@router.post("/register", response_model=schemas.Token)
+def register(subscriber: schemas.SubscriberCreate, db: Session = Depends(get_db)):
+    # Check if user exists
+    existing_user = crud.get_subscriber_by_email(subscriber.email, db)
+    if existing_user:
+        raise HTTPException(status_code=404, detail="User already exists")
+    
+    # Hash Password
+    hashed_password = security.hash_password(subscriber.password)
+    subscriber.password = hashed_password
+    
+    # Create user in DB
+    new_user = crud.create_new_subscriber(subscriber, db)
+    
+    # Return Token
+    token = security.create_access_token({"sub": new_user.email})
+    return {"access token": token, "token_type": "bearer"}
+
+
+@router.post("/login", reponse_model=schemas.Token)
+def login(subscriber: schemas.SubscriberLogin, db: Session = Depends(get_db)):
+    db_subscriber = crud.get_subscriber_by_email(subscriber.email, db)
+    if not db_subscriber:
+        raise HTTPException(status_code=404, detail="Invalid Credentials")
+    
+    token = security.create_access_token({"sub": db_subscriber.email})
+    return {"Access Token": token, "Token Type": "Bearer"}
--- app/routers/subscribers.py
+++ app/routers/subscribers.py
@@ -4,13 +4,12 @@ from sqlalchemy.orm import Session
 import app.crud as crud, app.schemas as schemas
 
 
-router = APIRouter()
+router = APIRouter(
+    prefix=("/subscribers"),
+    tags=["Subscribers"]
+)
 
-# Create an endpoint for creating a new subscriber and storing in database
-@router.post("/", response_model=schemas.SubscriberOut)
-def create_new_subscriber(subscriber: schemas.SubscriberCreate, db: Session=Depends(get_db)) -> schemas.SubscriberOut:
-    return crud.create_new_subscriber(subscriber, db)
-    
+
 # Endpoint for Returning all emails in database
 @router.get("/emails", response_model = list[schemas.SubscriberOut])
 def get_all_email(db: Session=Depends(get_db)):
