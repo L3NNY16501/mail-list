6eb65c6545a11db89d7a7702ce3a21d7aca11815
Ryan Leonard <ryanlfx16@gmail.com>
2026-02-25 23:19:40 +0000

    Formatted crud module

--- app/crud.py
+++ app/crud.py
@@ -16,7 +16,6 @@ def create_new_subscriber(subscriber: SubscriberCreate, db: Session) -> Subscrib
     db.refresh(new_subscriber)
     return new_subscriber
 
-
 # Get Subcriber by email
 def get_subscriber_by_email(email: str, db: Session) -> Subscriber:
     subscriber = db.query(Subscriber).filter(Subscriber.email == email).first()
@@ -28,12 +27,9 @@ def get_subscriber_by_email(email: str, db: Session) -> Subscriber:
 def verify_subscriber_password(password_input: str , db_subscriber: Subscriber) -> bool:
     return security.verify_password(password_input, db_subscriber.hashed_password)
         
-    
-  
 # Get all emails in database - Returns a list of Subscriber objects (records in table)
 def get_all_emails(db: Session) -> list[SubscriberOut]:
     return db.query(Subscriber).all()
-
 
 # Delete By Email
 def delete_subscriber(subscriber: Subscriber, db: Session) -> Subscriber:
